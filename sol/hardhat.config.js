b0c071cf0da9d4e87b859bb0e679f2ef8bd936ce
Luis Dominguez <ld@luis.sh>
2025-08-16 13:56:47 -0400

    Hardhat setup

---
+++ sol/hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: {
+        mnemonic: process.env.SEPOLIA_WALLET_MNEMONIC,
+      }
+    }
+  }
+};
