2c3c1b6e1c1df897a7d534c31a2698ec11d27f93
Luis Dominguez <ld@luis.sh>
2025-08-16 17:30:01 -0400

    Finished contracts

---
+++ sol/contracts/HoneyMarketplace.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./HoneyToken.sol";
+
+contract HoneyMarketplace {
+
+    struct RedemptionRequest {
+        address consumer;
+        uint256 amount;
+    }
+
+    //Beekeepers to the amount of honey they're offering for sale
+    mapping(address => uint256) public honeyMarketplace; 
+    //Beekeepers to their redemption requests
+    mapping(address => RedemptionRequest[]) public redemptionRequests;
+
+    uint constant public USD_HONEY_RATE = 1;
+    address constant public USDC_ADDRESS = 0x0000000000000000000000000000000000000000;
+    address constant public DAI_ADDRESS = 0x0000000000000000000000000000000000000000;
+    address immutable public HONEYTOKEN_ADDRESS;
+
+    // TODO: Refactor this later
+    modifier onlyBeekeeper {
+        require(HoneyToken(HONEYTOKEN_ADDRESS).beekeepers(msg.sender));
+        _;
+    } 
+
+    constructor(address honeytokenAddress) {
+        HONEYTOKEN_ADDRESS = honeytokenAddress;
+    }   
+
+    function offerHoney(uint256 amount) public onlyBeekeeper {
+        honeyMarketplace[msg.sender] += amount;
+        HoneyToken(HONEYTOKEN_ADDRESS).transferFrom(msg.sender, address(this), amount);
+    }
+
+    function buyHoney(address stablecoin, address beekeeper, uint256 honeyAmount) public {
+        require(stablecoin == USDC_ADDRESS || stablecoin == DAI_ADDRESS);
+        require(honeyAmount >= honeyMarketplace[beekeeper]);
+        ERC20(stablecoin).transferFrom(msg.sender, beekeeper, honeyAmount * USD_HONEY_RATE);
+        HoneyToken(HONEYTOKEN_ADDRESS).transfer(msg.sender, honeyAmount);
+        honeyMarketplace[beekeeper] -= honeyAmount;
+    }
+
+    function submitRedemption(address beekeeper, uint256 honeyAmount) public {
+        HoneyToken(HONEYTOKEN_ADDRESS).transferFrom(msg.sender, address(this), honeyAmount);
+        redemptionRequests[beekeeper].push(RedemptionRequest({
+            consumer: msg.sender, amount: honeyAmount
+        }));
+    }
+
+    function fulfillRedemption(uint256 consumerId, string calldata honeyId) public {
+        require(redemptionRequests[msg.sender].length != 0);
+        HoneyToken(HONEYTOKEN_ADDRESS).burn(redemptionRequests[msg.sender][consumerId].amount, honeyId);
+    }
+
+}
